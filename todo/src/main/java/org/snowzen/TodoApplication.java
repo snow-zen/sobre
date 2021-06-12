e8f20e215b5adaec14b208cf05d22071dd192d17
snow-zen <13517064839@139.com>
2021-06-12 23:10:33 +0800

    feat(All): Init project

---
+++ todo/src/main/java/org/snowzen/TodoApplication.java
@@ -0,0 +1,15 @@
+package org.snowzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author snow-zen
+ */
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class);
+    }
+}
