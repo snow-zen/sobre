2b8ab71bc2ced4f874b7a3757036ffeba49f9245
snow-zen <13517064839@139.com>
2021-06-12 23:13:47 +0800

    feat(Model): 添加TagPO

---
+++ todo/src/main/java/org/snowzen/model/po/TagPO.java
@@ -0,0 +1,26 @@
+package org.snowzen.model.po;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * 标签实体类
+ *
+ * @author snow-zen
+ */
+@Table(name = "tag")
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class TagPO extends BasePO {
+
+    /**
+     * 标签名
+     */
+    @Column
+    private String name;
+}
