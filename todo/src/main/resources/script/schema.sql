e7d346f0adcf2145ec3fd6d2dc7e76c7ef71d8d8
snow-zen <13517064839@139.com>
2021-06-13 13:12:59 +0800

    feat(resource): 添加schema.sql脚本

---
+++ todo/src/main/resources/script/schema.sql
@@ -0,0 +1,41 @@
+# 标签表
+CREATE TABLE TODO_TAG
+(
+    id   INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL COMMENT '标签名'
+);
+
+# 类型表
+CREATE TABLE TODO_CATEGORY
+(
+    id   INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL COMMENT '类型名'
+);
+
+# 任务表
+CREATE TABLE TODO_TASK
+(
+    id           INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    title        VARCHAR(100) NOT NULL COMMENT '任务标题',
+    content      TEXT COMMENT '任务内容',
+    finish_time  DATETIME COMMENT '任务预计完成时间',
+    is_active    TINYINT UNSIGNED COMMENT '任务是否启动',
+    gmt_create   DATETIME     NOT NULL DEFAULT NOW() COMMENT '任务创建时间',
+    gmt_modified DATETIME     NOT NULL DEFAULT NOW() COMMENT '任务更新时间'
+);
+
+# 任务-标签中间表
+CREATE TABLE TODO_TASK_TAG
+(
+    id      INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    task_id INT UNSIGNED,
+    tag_id  INT UNSIGNED
+) COMMENT '任务-标签中间表';
+
+# 任务-类型中间表
+CREATE TABLE TODO_TASK_CATEGORY
+(
+    id          INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    task_id     INT UNSIGNED,
+    category_id INT UNSIGNED
+) COMMENT '任务-类型中间表';
