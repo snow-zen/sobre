73c0d398592230ade05e130ef130092bee2fb98e
snow-zen <13517064839@139.com>
2021-06-13 14:25:15 +0800

    fix(Model): 添加主键id生成策略

--- todo/src/main/java/org/snowzen/model/po/BasePO.java
+++ todo/src/main/java/org/snowzen/model/po/BasePO.java
@@ -2,6 +2,8 @@ package org.snowzen.model.po;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
@@ -18,6 +20,7 @@ public abstract class BasePO {
      * 主键id
      */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
 
 }
