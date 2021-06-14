d4fc477b720cc661b5506e17a2206b56e90a06e7
snow-zen <13517064839@139.com>
2021-06-14 23:12:18 +0800

    feat(service): 分类服务添加单记录查询方法

--- todo/src/main/java/org/snowzen/service/CategoryService.java
+++ todo/src/main/java/org/snowzen/service/CategoryService.java
@@ -1,7 +1,12 @@
 package org.snowzen.service;
 
+import org.snowzen.base.IdUtil;
+import org.snowzen.exception.NotFoundDataException;
+import org.snowzen.model.dto.CategoryDTO;
 import org.snowzen.repository.dao.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 /**
  * 分类服务
@@ -15,5 +20,13 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public CategoryDTO findById(int id) {
+        checkArgument(IdUtil.checkId(id), "无效id");
+
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundDataException("分类不存在"))
+                .convert();
     }
 }
