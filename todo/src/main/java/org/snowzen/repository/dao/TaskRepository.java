1fbd9797e8d56643cd697d24a242af0bdbb6e736
snow-zen <13517064839@139.com>
2021-06-17 22:53:22 +0800

    feat(repository): 任务服务添加通过分类id查询关联任务

--- todo/src/main/java/org/snowzen/repository/dao/TaskRepository.java
+++ todo/src/main/java/org/snowzen/repository/dao/TaskRepository.java
@@ -2,6 +2,8 @@ package org.snowzen.repository.dao;
 
 import org.snowzen.model.po.TaskPO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,4 +13,14 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface TaskRepository extends JpaRepository<TaskPO, Integer> {
+
+    /**
+     * 通过分类id查询对应关联任务
+     *
+     * @param categoryId 分类id
+     * @return 查询到的关联任务列表
+     */
+    @Query(value = "SELECT TT.id AS id, title, content, finish_time, is_active, review_strategy, gmt_create, gmt_modified, TTC.id, task_id, category_id FROM TODO_TASK TT LEFT JOIN TODO_TASK_CATEGORY TTC on TT.id = TTC.task_id WHERE TT.id = :categoryId",
+            nativeQuery = true)
+    Iterable<TaskPO> findAllByCategoryId(@Param("categoryId") int categoryId);
 }
