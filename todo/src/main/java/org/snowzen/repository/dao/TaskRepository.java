08c60660fcf65aeb657ba2e136b91f426762ea55
snow-zen <13517064839@139.com>
2021-06-19 16:20:04 +0800

    feat(TaskRepository): 支持模糊查询、复习时间查询

--- todo/src/main/java/org/snowzen/repository/dao/TaskRepository.java
+++ todo/src/main/java/org/snowzen/repository/dao/TaskRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * {@link TaskPO}对应JPA库
  *
@@ -23,4 +26,20 @@ public interface TaskRepository extends JpaRepository<TaskPO, Integer> {
     @Query(value = "SELECT TT.id AS id, title, content, finish_time, is_active, review_strategy, gmt_create, gmt_modified, TTC.id, task_id, category_id FROM TODO_TASK TT LEFT JOIN TODO_TASK_CATEGORY TTC on TT.id = TTC.task_id WHERE TT.id = :categoryId",
             nativeQuery = true)
     Iterable<TaskPO> findAllByCategoryId(@Param("categoryId") int categoryId);
+
+    /**
+     * 通过关键字匹配对应任务
+     *
+     * @param key 关键字
+     * @return 匹配到的任务列表
+     */
+    List<TaskPO> findAllByTitleLike(String key);
+
+    /**
+     * 查询复习时间在指定时间前的任务
+     *
+     * @param time 指定时间
+     * @return 查询到的任务列表
+     */
+    List<TaskPO> findAllByFinishTimeBefore(LocalDateTime time);
 }
