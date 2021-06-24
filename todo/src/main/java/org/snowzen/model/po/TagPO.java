ff5d600bd524ce952bd613fa5693abbe35935f7a
snow-zen <13517064839@139.com>
2021-06-24 22:55:43 +0800

    标签转换改为Assembler

--- todo/src/main/java/org/snowzen/model/po/TagPO.java
+++ todo/src/main/java/org/snowzen/model/po/TagPO.java
@@ -2,9 +2,6 @@ package org.snowzen.model.po;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.snowzen.model.Convertible;
-import org.snowzen.model.dto.TagDTO;
-import org.springframework.beans.BeanUtils;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -19,7 +16,7 @@ import javax.persistence.Table;
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class TagPO extends BasePO implements Convertible<TagDTO> {
+public class TagPO extends BasePO {
 
     /**
      * 标签名
@@ -27,15 +24,4 @@ public class TagPO extends BasePO implements Convertible<TagDTO> {
     @Column
     private String name;
 
-    @Override
-    public TagDTO convert() {
-        TagDTO tagDTO = new TagDTO();
-        BeanUtils.copyProperties(this, tagDTO);
-        return tagDTO;
-    }
-
-    @Override
-    public void reverse(TagDTO tagDTO) {
-        BeanUtils.copyProperties(tagDTO, this);
-    }
 }
