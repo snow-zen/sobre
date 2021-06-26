cd4c01b7cbf4f00cba1516c66fe063eefb37e98a
snow-zen <13517064839@139.com>
2021-06-26 21:36:25 +0800

    feat(model): TaskDTO添加JSR-303校验规则

--- todo/src/main/java/org/snowzen/model/dto/TaskDTO.java
+++ todo/src/main/java/org/snowzen/model/dto/TaskDTO.java
@@ -2,7 +2,13 @@ package org.snowzen.model.dto;
 
 import lombok.Data;
 import org.snowzen.review.ReviewStrategy;
+import org.snowzen.support.validation.ValidGroup;
+import org.snowzen.support.validation.constraints.ValidId;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,20 +21,27 @@ import java.util.List;
 @Data
 public class TaskDTO implements Serializable {
 
+    @ValidId
     private Integer id;
 
+    @NotBlank
     private String title;
 
     private String content;
 
+    @Future(groups = ValidGroup.ModifyGroup.class)
     private LocalDateTime finishTime;
 
+    @NotNull(groups = ValidGroup.ModifyGroup.class)
     private Boolean active;
 
+    @NotNull
     private ReviewStrategy reviewStrategy;
 
+    @PastOrPresent(groups = ValidGroup.ModifyGroup.class)
     private LocalDateTime createTime;
 
+    @PastOrPresent(groups = ValidGroup.ModifyGroup.class)
     private LocalDateTime modifiedTime;
 
     private List<CategoryDTO> categories;
