c1b197cabc8587625a3b985f8638054e034ada91
snow-zen <13517064839@139.com>
2021-07-03 02:38:21 +0800

    feat(controller): 完善controller控制器

--- todo/src/main/java/org/snowzen/controller/CategoryController.java
+++ todo/src/main/java/org/snowzen/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package org.snowzen.controller;
 
+import org.snowzen.model.assembler.CategoryAssembler;
+import org.snowzen.model.command.CategoryCreateCommand;
 import org.snowzen.model.dto.CategoryDTO;
 import org.snowzen.service.CategoryService;
+import org.snowzen.support.validation.constraints.ValidId;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +15,24 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/category")
+@Validated
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final CategoryAssembler categoryAssembler;
 
-    public CategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService, CategoryAssembler categoryAssembler) {
         this.categoryService = categoryService;
+        this.categoryAssembler = categoryAssembler;
     }
 
     @GetMapping("{categoryId}")
-    public CategoryDTO category(@PathVariable("categoryId") int categoryId) {
+    public CategoryDTO category(@PathVariable("categoryId") @ValidId(message = "无效id") int categoryId) {
         return categoryService.findCategoryById(categoryId);
     }
 
     @PostMapping
-    public void category(@RequestBody @Validated CategoryDTO categoryDTO) {
-        categoryService.addCategory(categoryDTO);
+    public void category(@RequestBody @Validated CategoryCreateCommand command) {
+        categoryService.addCategory(categoryAssembler.toDTO(command));
     }
 }
--- todo/src/main/java/org/snowzen/controller/TaskController.java
+++ todo/src/main/java/org/snowzen/controller/TaskController.java
@@ -1,8 +1,11 @@
 package org.snowzen.controller;
 
+import org.snowzen.model.assembler.TaskAssembler;
+import org.snowzen.model.command.TaskCreateCommand;
+import org.snowzen.model.command.TaskUpdateCommand;
 import org.snowzen.model.dto.TaskDTO;
 import org.snowzen.service.TaskService;
-import org.snowzen.support.validation.ValidGroup.ModifyGroup;
+import org.snowzen.support.validation.constraints.ValidId;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,26 +18,29 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/task")
+@Validated
 public class TaskController {
 
     private final TaskService taskService;
+    private final TaskAssembler taskAssembler;
 
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, TaskAssembler taskAssembler) {
         this.taskService = taskService;
+        this.taskAssembler = taskAssembler;
     }
 
     @GetMapping("{taskId}")
-    public TaskDTO task(@PathVariable("taskId") int taskId) {
+    public TaskDTO task(@PathVariable("taskId") @ValidId(message = "无效任务id") int taskId) {
         return taskService.findTask(taskId);
     }
 
     @PostMapping
-    public void task(@RequestBody @Validated TaskDTO taskDTO) {
-        taskService.addTask(taskDTO);
+    public void task(@RequestBody @Validated TaskCreateCommand command) {
+        taskService.addTask(taskAssembler.toDTO(command));
     }
 
     @GetMapping("/list/category/{categoryId}")
-    public List<TaskDTO> taskListByCategoryId(@PathVariable("categoryId") int categoryId) {
+    public List<TaskDTO> taskListByCategoryId(@PathVariable("categoryId") @ValidId(message = "无效分类id") int categoryId) {
         return taskService.findAllByCategoryId(categoryId);
     }
 
@@ -49,17 +55,17 @@ public class TaskController {
     }
 
     @PutMapping("/review")
-    public void taskReview(@RequestParam("taskId") int taskId) {
+    public void taskReview(@RequestParam("taskId") @ValidId(message = "无效任务id") int taskId) {
         taskService.finishReview(taskId);
     }
 
     @PutMapping
-    public void taskModify(@RequestBody @Validated(ModifyGroup.class) TaskDTO taskDTO) {
-        taskService.modify(taskDTO);
+    public void taskModify(@RequestBody @Validated TaskUpdateCommand command) {
+        taskService.modify(taskAssembler.toDTO(command));
     }
 
     @DeleteMapping("{taskId}")
-    public void taskDelete(@PathVariable("taskId") int taskId) {
+    public void taskDelete(@PathVariable("taskId") @ValidId(message = "无效任务id") int taskId) {
         taskService.delete(taskId);
     }
 
