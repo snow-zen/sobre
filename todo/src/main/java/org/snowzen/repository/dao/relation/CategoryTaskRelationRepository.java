c91665e341c7097518e96cb8afe4f9d7a031eaaf
snow-zen <13517064839@139.com>
2021-07-05 22:02:16 +0800

    feat(repository): 支持多任务id查询对应关联分类关系

--- todo/src/main/java/org/snowzen/repository/dao/relation/CategoryTaskRelationRepository.java
+++ todo/src/main/java/org/snowzen/repository/dao/relation/CategoryTaskRelationRepository.java
@@ -4,6 +4,7 @@ import org.snowzen.model.po.relation.CategoryTaskRelationPO;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -18,9 +19,17 @@ public interface CategoryTaskRelationRepository extends CrudRepository<CategoryT
      * 通过任务id查询关联实体
      *
      * @param taskId 任务id
-     * @return 关联实体表
+     * @return 关联实体列表
      */
     List<CategoryTaskRelationPO> findAllByTaskId(int taskId);
+
+    /**
+     * 通过任务id列表查询关联实体列表
+     *
+     * @param taskIdList 任务id列表
+     * @return 关联实体列表
+     */
+    List<CategoryTaskRelationPO> findAllByTaskIdIn(Collection<Integer> taskIdList);
 
     /**
      * 通过任务id删除关联实体
