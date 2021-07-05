b2a984fcc24431338875f1e033f330377ef6643c
snow-zen <13517064839@139.com>
2021-07-05 22:12:17 +0800

    feat(service): 支持多标签id匹配标签

--- todo/src/main/java/org/snowzen/service/TagService.java
+++ todo/src/main/java/org/snowzen/service/TagService.java
@@ -1,7 +1,14 @@
 package org.snowzen.service;
 
+import org.snowzen.model.assembler.TagAssembler;
+import org.snowzen.model.dto.TagDTO;
 import org.snowzen.repository.dao.TagRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * 标签服务
@@ -12,8 +19,22 @@ import org.springframework.stereotype.Service;
 public class TagService {
 
     private final TagRepository tagRepository;
+    private final TagAssembler tagAssembler;
 
-    public TagService(TagRepository tagRepository) {
+    public TagService(TagRepository tagRepository, TagAssembler tagAssembler) {
         this.tagRepository = tagRepository;
+        this.tagAssembler = tagAssembler;
+    }
+
+    /**
+     * 通过标签id列表查询匹配标签
+     *
+     * @param tagIdList 标签id列表
+     * @return 匹配标签列表
+     */
+    public List<TagDTO> findAllTagById(Collection<Integer> tagIdList) {
+        return StreamSupport.stream(tagRepository.findAllById(tagIdList).spliterator(), false)
+                .map(tagAssembler::toDTO)
+                .collect(Collectors.toList());
     }
 }
