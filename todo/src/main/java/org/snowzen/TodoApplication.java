b4492fff3d066b2fb9a7d62ee5b5b424e6a509d5
snow-zen <13517064839@139.com>
2021-07-30 20:52:55 +0800

    去除不必要的配置

--- todo/src/main/java/org/snowzen/TodoApplication.java
+++ todo/src/main/java/org/snowzen/TodoApplication.java
@@ -2,11 +2,16 @@ package org.snowzen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 
 /**
  * @author snow-zen
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        WebSocketServletAutoConfiguration.class,
+        JmxAutoConfiguration.class
+})
 public class TodoApplication {
 
     public static void main(String[] args) {
